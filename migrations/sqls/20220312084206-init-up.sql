8dd14caa390476ef52506a1561ffc505329d5402
h4nyu <yao.ntno@gmail.com>
2022-03-12 19:12:33 +0900

    brash up

--- migrations/sqls/20220312084206-init-up.sql
+++ migrations/sqls/20220312084206-init-up.sql
@@ -1,1 +1,11 @@
 /* Replace with your SQL commands */
+
+CREATE TABLE orders (
+    id             text        NOT NULL PRIMARY KEY,
+    contract_price number,
+    kind           text        NOT NULL,
+    status         text        NOT NULL,
+    side           text        NOT NULL,
+    updated_at     TIMESTAMPTZ NOT NULL,
+    created_at     TIMESTAMPTZ NOT NULL
+);
