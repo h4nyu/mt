1cee617b9e2a848aca71bc05b397fe9e3d865ac3
h4nyu <yao.ntno@gmail.com>
2022-03-12 18:06:01 +0900

    update

--- packages/core/candle.ts
+++ packages/core/candle.ts
@@ -1,6 +1,7 @@
 import { nanoid } from 'nanoid';
 export type Candle = {
   id: string
+  symbolId: string
   open: number
   close: number
   high: number
@@ -12,9 +13,10 @@ export type Candle = {
 export const Candle = (props: Omit<Candle, "id"|"time"> & { id?:string, time?:Date}): Candle => {
   const id = props.id ?? nanoid();
   const time = props.time ?? new Date();
-  const { open, close, high, low, volume } = props;
+  const { open, close, high, low, volume, symbolId } = props;
   return {
     id,
+    symbolId,
     open,
     close,
     high,
