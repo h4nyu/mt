5ba9d1ab460e1e1390711a95fb8c3861f4f89f07
h4nyu <yao.ntno@gmail.com>
2022-03-12 18:53:48 +0900

    fix

--- packages/core/order.ts
+++ packages/core/order.ts
@@ -10,10 +10,18 @@ export enum PositionKind {
   Short = 'Short',
 }
 
+// TODO
+export enum OrderStatus {
+  New = 'New',
+  Canceled = 'Canceled',
+}
+
 export type Order = {
   id: string
   createdAt: Date
   positionKind: PositionKind
+  contractPrice?: number,
+  status: OrderStatus,
 } | {
   kind: OrderKind.Limit,
   price: number
