d74282829c1300889275b2742993f3e1a2617761
XinyuanYao <yao.ntno@gmail.com>
2023-07-17 09:58:28 +0000

    add board domain

--- core/board.ts
+++ core/board.ts
@@ -1,3 +1,28 @@
-export type Board {
-
+export type BoardRow = {
+  price: number;
+  amount: number;
+}
+export type Board = {
+  symbol: string;
+  sell: BoardRow[];
+  buy: BoardRow[];
+  current: {
+    price: number;
+    time: Date;
+  },
+  over: {
+    sellAmount: number;
+    buyAmount: number;
+    sign: number;
+  },
+  ask: {
+    amount: number;
+    price: number;
+    time: Date;
+  },
+  bid: {
+    amount: number;
+    price: number;
+    time: Date;
+  }
 }
