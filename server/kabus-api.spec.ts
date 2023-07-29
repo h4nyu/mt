22667c2020bc0783164227f4cbf3c3eb51d5bfc0
XinyuanYao <yao.ntno@gmail.com>
2023-07-29 17:05:21 +0000

    add subscribe

--- server/kabus-api.spec.ts
+++ server/kabus-api.spec.ts
@@ -8,10 +8,19 @@ describe('KabusApi', () => {
     })
     return { client }
   }
-  test("refleshToken", async () => {
+  describe('auth', () => {
+    test("refleshToken", async () => {
+      const { client } = setup()
+      const res = await client.auth.refleshToken()
+      if(res instanceof Error) throw res
+        expect(client.auth.http.defaults.headers.common['X-API-KEY']).toBeTruthy()
+    })
+  })
+  test("register", async () => {
     const { client } = setup()
-    const res = await client.refleshToken()
+    const res = await client.register({
+      symbols: ['9433', '9437'] // 任天堂, グリー
+    })
     if(res instanceof Error) throw res
-    expect(client.auth.token).not.toBe('')
   })
 })
--- server/kabus-api.ts
+++ server/kabus-api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Logger } from "@kgy/core/logger"
-export const KabusApi = (props?: {
+
+const Auth = (props?: {
   logger?: Logger
 }) => {
   const http = axios.create({
@@ -10,22 +11,56 @@ export const KabusApi = (props?: {
     module: 'KabusApi',
     message: `Connect to ${process.env.KABUSAPI_URL}`
   })
-  const auth = {
-    token: '',
-  }
   const refleshToken = async () => {
     try{
       const res = await http.post('/token', {
         APIPassword: process.env.KABUSAPI_PASSWORD,
       })
-      auth.token = res.data.Token
+      http.defaults.headers.common['X-API-KEY'] = res.data.Token
     }catch(e){
       return e
     }
   }
+  const getClient = async () => {
+    if(!http.defaults.headers.common['X-API-KEY']){
+      const err = await refleshToken()
+      if(err) return err
+    }
+    return http
+  }
   return {
+    getClient,
     refleshToken,
+    http,
+  }
+}
+export const KabusApi = (props?: {
+  logger?: Logger
+}) => {
+  const auth = Auth(props)
+  const register = async (req: {
+    symbol: string[]
+  }) => {
+    const { symbols } = req
+    const http = await auth.getClient()
+    if(http instanceof Error) return http
+    try{
+      const res = await http.put('/register', { 
+        Symbols: symbols.map((symbol) => {
+          return {
+            Symbol: symbol,
+            Exchange: 1, // 東証
+          }
+        }),
+      })
+      return
+    }
+    catch(e){
+      return e
+    }
+  }
+  return {
+    register,
     auth,
   }
-
 }
