a4c9e948e69cb030d9a085a7198de7ccbbc59468
XinyuanYao <yao.ntno@gmail.com>
2023-08-08 15:07:36 +0000

    add missing type

--- usecase/notify-change.ts
+++ usecase/notify-change.ts
@@ -1,3 +1,5 @@
+import { Logger } from "@kgy/core/logger";
+
 export const NotifyChangeFn = (props: { logger?: Logger }) => {
   const run = async (req: { code: string; limit?: number }) => {};
   return {
